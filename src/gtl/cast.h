4b7d09355f4a4901c8c0baef8b74d0de30b79e75
Guillaume Arruda <guillaume.arruda@gmail.com>
2020-02-12 20:05:36 -0500

    add gtl::cast to add possiblity to have conditionnaly checked static cast

---
+++ src/gtl/cast.h
@@ -0,0 +1,36 @@
+#pragma once
+#include <utility>
+#include <type_traits>
+
+#define GTL_CHECK_CAST() 1
+
+namespace gtl
+{
+    namespace details
+    {
+        template<class To, class From>
+        void check_valid_cast(From&& from)
+        {
+#if GTL_CHECK_CAST()
+            if constexpr (std::is_polymorphic_v<std::remove_pointer_t<std::decay_t<To>>> && std::is_polymorphic_v<std::remove_pointer_t<std::decay_t<From>>>)
+            {
+                if constexpr (std::is_pointer_v<std::decay_t<From>>)
+                {
+                    if (from != nullptr)
+                        dynamic_cast<std::add_lvalue_reference_t<std::remove_pointer_t<To>>>(*std::forward<From>(from));
+                }
+                else
+                {
+                    dynamic_cast<To>(std::forward<From>(from));
+                }
+            }
+#endif
+        }
+    }
+    template<class To, class From>
+    auto cast(From&& from)
+    {
+        details::check_valid_cast<To>(std::forward<From>(from));
+        return static_cast<To>(std::forward<From>(from));
+    }
+}
